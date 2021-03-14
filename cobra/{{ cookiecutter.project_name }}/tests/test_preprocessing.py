955276e41fb351a655051192fd94edcdf4e72df6
Kris Peeters <kris@dataminded.be>
2021-03-14 21:22:17 +0100

    move cookiecutter back up

--- cobra/{{ cookiecutter.project_name }}/tests/test_preprocessing.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from {{ cookiecutter.project_name }}.jobs import preprocessing
-
-
-def test_basetable_is_created_from_csv_file():
-    df = pd.read_csv("tests/resources/earnings_dataset.csv")
-    basetable = preprocessing.preprocess(df)
-    assert basetable is not None
